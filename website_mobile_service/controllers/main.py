db216427a3b0ed5a21985980ec1a208ec66d8ffd
mostafabarmshory <mostafa.barmshory@gmail.com>
2023-01-27 14:32:20 +0330

    Show a list of brands

--- website_mobile_service/controllers/main.py
+++ website_mobile_service/controllers/main.py
@@ -7,11 +7,13 @@ class MobileService(Controller):
 
     @route('/mobile-service', type='http', auth="public", website=True, sitemap=True)
     def index(self):
-        Brand = request.env['blog.blog']
+        Brand = request.env['mobile.brand']
+        brands = Brand.search([(1, "=", 1)])
 
 
-
-        values = {}
+        values = {
+            'brands': brands
+        }
 
 
         return request.render("website_mobile_service.brand_select_wizard", values)
