b945db2f4053891f9d3380273f2874f11122d7ef
mostafabarmshory <mostafa.barmshory@gmail.com>
2023-01-27 14:12:49 +0330

    featuer: website integeration is started

---
+++ website_mobile_service/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Mobile Service Management Website Integeration',
+    'version': '16.0.1.0.0',
+    'summary': 'Module for connecting website into mobile service.',
+    'category': 'Website',
+    'author': 'ViraWeb123',
+    'company': 'ViraWeb123',
+    'website': 'https://viraweb123.ir',
+    'depends': [
+        'website',
+        'mobile_service',
+    ],
+    'data': [
+        'views/website_brand_templates.xml'
+    ],
+    'images': [],
+    'assets': {},
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
