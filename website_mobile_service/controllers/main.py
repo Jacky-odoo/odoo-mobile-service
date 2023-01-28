b41c26b0a675a47b74846d30bb5da1becb37c028
mostafabarmshory <mostafa.barmshory@gmail.com>
2023-01-28 15:50:31 +0330

    update: send form to backend

--- website_mobile_service/controllers/main.py
+++ website_mobile_service/controllers/main.py
@@ -4,7 +4,12 @@ from odoo.http import Controller, route, request
 
 class MobileService(Controller):
 
-    @route('/mobile-service', type='http', auth="public", website=True, sitemap=True)
+    @route('/mobile-service',
+           type='http',
+           auth="public",
+           website=True,
+           sitemap=True,
+           methods=['GET'])
     def brand_select(self):
         Brand = request.env['mobile_service.brand']
         brands = Brand.search([(1, "=", 1)])
@@ -14,7 +19,12 @@ class MobileService(Controller):
         }
         return request.render("website_mobile_service.brand_select_wizard", values)
 
-    @route('/mobile-service/<string:ref>', type='http', auth="public", website=True, sitemap=True)
+    @route('/mobile-service/<string:ref>',
+           type='http',
+           auth="public",
+           website=True,
+           sitemap=True,
+           methods=['GET'])
     def brand_model_select(self, ref):
         brand = self._get_brand(ref)
         return request.render("website_mobile_service.brand_model_select_wizard", {
@@ -26,7 +36,8 @@ class MobileService(Controller):
            type='http',
            auth="public",
            website=True,
-           sitemap=True)
+           sitemap=True,
+           methods=['GET'])
     def complaint_select(self, brand_ref, model_ref):
         Complaint = request.env['mobile_service.complaint']
         return request.render("website_mobile_service.complaint_select_wizard", {
@@ -39,7 +50,8 @@ class MobileService(Controller):
            type='http',
            auth="public",
            website=True,
-           sitemap=True)
+           sitemap=True,
+           methods=['GET'])
     def complaint_description_select(self, brand_ref, model_ref, complaint_ref):
         complaint = self._get_complaint(complaint_ref)
         return request.render("website_mobile_service.complaint_description_select_wizard", {
@@ -53,14 +65,25 @@ class MobileService(Controller):
            type='http',
            auth="public",
            website=True,
-           sitemap=True)
-    def register_issue(self, brand_ref, model_ref, complaint_ref, complaint_description_ref):
+           sitemap=True,
+           methods=['GET'])
+    def register_issue_form(self, brand_ref, model_ref, complaint_ref, complaint_description_ref):
         return request.render("website_mobile_service.request_submit_wizard", {
             'brand': self._get_brand(brand_ref),
             'model': self._get_brand_model(model_ref),
             'complaint': self._get_complaint(complaint_ref),
             'complaint_description': self._get_complaint_description(complaint_description_ref)
         })
+
+
+    @route('/mobile-service/submit_service_request',
+           type='http',
+           auth="public",
+           website=True,
+           sitemap=False,
+           methods=['POST'])
+    def register_issue(self, **kw):
+        return request.render("website_mobile_service.request_submit_success_wizard", {})
 
     def _get_brand(self, ref):
         return self._get_model_by_ref('mobile_service.brand', ref)
