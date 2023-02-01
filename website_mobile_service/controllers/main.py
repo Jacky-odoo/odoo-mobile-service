29a53730e45887abe739ae3a2f2f9ec5e9177935
Fardin Mardani <fmardani0@gmail.com>
2023-02-01 18:25:23 +0330

    rename submit input fields and create dynamic title to crm

--- website_mobile_service/controllers/main.py
+++ website_mobile_service/controllers/main.py
@@ -82,10 +82,23 @@ class MobileService(Controller):
            sitemap=False,
            methods=['POST'])
     def register_issue(self, **kw):
-        # TODO: maso, submet as CRM or repair service
+        # maso, submet as CRM or repair service
+
+        brand = self._get_brand(kw['brand_ref'] if 'brand_ref' in kw else '')
+        mode = self._get_brand_model(
+            kw['model_ref'] if 'model_ref' in kw else '')
+        complaint = self._get_complaint(
+            kw['complaint_ref'] if 'complaint_ref' in kw else '')
+        complaint_description = self._get_complaint_description(
+            kw['complaint_description_ref'] if 'complaint_description_ref' in kw else '')
+
+        gen_name = kw['contact_name'] + \
+            '( '+brand.name+':'+mode.name+':' + \
+            complaint.name+':'+complaint_description.name+')'
+
         Model = request.env['crm.lead']
-        Model.create({
-            'name': kw['name'],
+        Model.sudo().create({
+            'name': gen_name,
             'street': kw['street'],
             'phone': kw['phone'],
             'email_from': kw['email_from'],
