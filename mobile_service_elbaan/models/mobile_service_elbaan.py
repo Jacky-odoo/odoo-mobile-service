f07f6499416f3aece3db460c6b30c5915710c77a
fardinmardani <fmardani0@gmail.com>
2023-03-12 09:19:51 +0330

    اضافه کردن ساب استیت برای ارزیابی

--- mobile_service_elbaan/models/mobile_service_elbaan.py
+++ mobile_service_elbaan/models/mobile_service_elbaan.py
@@ -1,28 +1,38 @@
-from odoo import models, _
+from odoo import models,fields, _
 from datetime import datetime
+
+
 class MobileServiceElbaanShop(models.Model):
     _inherit = ['mobile_service.service']
+    substate_evaluation = fields.Selection(
+        selection=[('evaluation', 'Evaluation'),
+            ('ppsupply', 'Parts/phone supply'),
+            ('repairs', 'Repairs')],
+        string='Evaluation Sub States',
+        track_visibility='always',
+        default='evaluation',
+        tracking=True)
 
 
- ################################################################################
+################################################################################
     #              State Machin: Actions
     ################################################################################
 
+    # States that we create ---------->
 
-    # States that we create ----------> 
+
     def action_draftmobile_service(self):
         """
         this is called when a record set in first level
         """
         self.service_state = 'draft'
+
     def action_accept_service(self):
         """
         this is called when a record set in second level
         """
         self.accept_date = datetime.now()
         self.service_state = 'accepted'
-
-
 
     def action_acceptmobile_service(self):
         """
