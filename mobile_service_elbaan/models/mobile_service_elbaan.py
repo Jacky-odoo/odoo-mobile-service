2681f474fe322f49546b84fb8d069a472da84a30
fardinmardani <fmardani0@gmail.com>
2023-03-14 16:14:15 +0330

    create function fetch from chatter and change regex for website

--- mobile_service_elbaan/models/mobile_service_elbaan.py
+++ mobile_service_elbaan/models/mobile_service_elbaan.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, _
 from datetime import datetime
+import re
 
 
 class MobileServiceElbaanShop(models.Model):
@@ -53,5 +54,13 @@ class MobileServiceElbaanShop(models.Model):
              self.substate_evaluation = 'ppsupply'
         else:
              self.service_state = 'customer'
-
-    
+#!this function read from chatter body of memo ang delete tags and insert in to internal_note
+    def action_get_chat_messages(self):
+       regex = re.compile(r'<[^>]+>')
+       self.internal_notes= ""
+       messa = self.env['mail.message'].search([('res_id', "=", self.id),('subtype_id',"=", 2),('record_name',"=", self.name), ('model', "=", "mobile_service.service"),], order='create_date asc')
+       if messa:
+            for doc in messa:
+                self.internal_notes+=regex.sub('\t',doc.body)
+       else:
+           self.internal_notes="is nothing"
