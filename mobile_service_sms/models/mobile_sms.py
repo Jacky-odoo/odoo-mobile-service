9e2f53af0f2648686c22d177f5323be7a77db896
fardinmardani <fmardani0@gmail.com>
2023-04-04 08:41:00 +0330

    system sms is finish asanak and smsir

--- mobile_service_sms/models/mobile_sms.py
+++ mobile_service_sms/models/mobile_sms.py
@@ -1,92 +1,133 @@
 from odoo import models, fields, api, _
 from odoo.addons.iap.tools import iap_tools
+from odoo.addons.sms.models.sms_api import SmsApi
 import requests
-
+import json
 
 
 class MobileSms(models.AbstractModel):
     _inherit = "sms.api"
-    
-    @api.model
-    def _send_sms(self, numbers, message):
-        """ Send a single message to several numbers
 
-        :param numbers: list of E164 formatted phone numbers
-        :param message: content to send
-
-        :raises ? TDE FIXME
-        """
-
-        serv = self.env['res.config.settings'].search([('server_special', '=', True)],
-                                                            limit=1,
-                                                            order="create_date DESC")
-        
-        if serv[0]:
-            if serv[0].server_id.id == 1:
-                serv1 = self.env['mobile_sms.server'].search([('id', '=', '1')])
-                url = serv1.url
-                payload = {
-                    "username": serv1.user_name,
-                    "password": serv1.password,
-                    "Source": serv1.source_tel,
-                    "Message": message,
-                    "destination": ','.join(numbers)
-                }
-                response = requests.request("POST", url, data=payload, timeout=5)
-                return response
-            if serv[0].server_id.id == 2:
-                head = {
-                    "X-API-KEY": "9125456459@elbaan.com!",
-                    'ACCEPT': 'application/json',
-                    'Content-Type': 'application/json',
-                }
-                serv2 = self.env['mobile_sms.server'].search([('id', '=', '2')])
-                url = serv2.url
-                payload = {
-                    "username": serv2.user_name,
-                    "password": serv2.password,
-                    "lineNumber": serv2.source_tel,
-                    "MessageText": message,
-                    "Mobiles": ','.join(numbers)
-                }
-                response = requests.request("POST", url, headers=head, json=payload)
-                return response
-        else:
-             params = {
-            'numbers': numbers,
-            'message': message,
-        }
-        return self._contact_iap('/iap/message_send', params)
-        
-
-        # params = {
-        #    'numbers': numbers,
-        #    'message': message,
-        # }
-        # return self._contact_iap('/iap/message_send', params)
 
     @api.model
     def _send_sms_batch(self, messages):
-        """ Send SMS using IAP in batch mode
-
+        """
         :param messages: list of SMS to send, structured as dict [{
             'res_id':  integer: ID of sms.sms,
             'number':  string: E164 formatted phone number,
             'content': string: content to send
         }]
-
-        :return: return of /iap/sms/1/send controller which is a list of dict [{
-            'res_id': integer: ID of sms.sms,
-            'state':  string: 'insufficient_credit' or 'wrong_number_format' or 'success',
-            'credit': integer: number of credits spent to send this SMS,
-        }]
-
-        :raises: normally none
         """
-        for message in messages:
-            self._send_sms([message['number']], message['content'])
+        params = self.env['ir.config_parameter'].sudo()
+        if params.get_param('mobile_sms.server_special'):
+            server_config_id = params.get_param('mobile_sms.server_id')
+            server_config = self.env['mobile_sms.server'].search(
+                [('id', '=', server_config_id)])
+        else:
+            server_config = False
 
-        params = {
-            'messages': messages
+        if server_config and server_config.server_type == 'asanak':
+            response = self._send_by_asanak(server_config, messages)
+        elif server_config and server_config.server_type == 'smsir':
+            response = self._send_by_smsir(server_config, messages)
+        else:
+            response = super()._send_sms_batch(messages)
+        return response
+
+
+    def _send_by_asanak(self, confg, messages):
+        result = []
+        for message in messages:
+            payload = {
+                "username": confg.user_name,
+                "password": confg.password,
+                "Source": confg.source_tel,
+                "Message": message['content'],
+                "destination": message['number']
+            }
+            response = requests.request(
+                "POST",
+                url='https://panel.asanak.com/webservice/v1rest/sendsms',
+                data=payload,
+                timeout=5)
+            if response.status_code == 200:
+                if (response.content != 'Source Number is not valid') and (response.content != 'Username or Password is not valid'):
+                    result.append({
+                        'res_id': message['res_id'],
+                        'state': 'success',
+                        'credit': 5,
+                    })
+            else:
+                result.append({
+                    'res_id': message['res_id'],
+                    'state': 'Un-success',
+                    'credit': 5,
+                })
+        return result
+
+    def _send_by_smsir(self, config, messages):
+        result = []
+        
+        for message in messages:
+            api_key = self.get_token_from_smsir(
+                UserApiKey=config.api_key,
+                SecretKey=config.security_code
+            )
+            
+            headers = {
+                'Content-type': 'application/json',
+                'x-sms-ir-secure-token': api_key,
+            }
+            body = {
+                "Messages": [message['content']],
+                "MobileNumbers": [message['number']],
+                "LineNumber": config.source_tel,
+                "SendDateTime": "",
+                "CanContinueInCaseOfError": "false",
+            }
+            response = requests.post(
+                'https://RestfulSms.com/api/MessageSend', 
+                data=json.dumps(body),
+                headers=headers
+            )
+            if response.status_code == 201:
+                if response.json()['IsSuccessful'] == True:  
+                    result.append({
+                        'res_id': message['res_id'],
+                        'state': 'success',
+                        'credit': 5,
+                    })
+                else:
+                    result.append({
+                    'res_id': message['res_id'],
+                    'state': 'Un-success',
+                    'credit': 5,
+                })
+            else:
+                result.append({
+                    'res_id': message['res_id'],
+                    'state': 'Un-success',
+                    'credit': 5,
+                })
+        return result
+
+
+
+    def get_token_from_smsir(self, UserApiKey='', SecretKey=''):
+        url = 'http://RestfulSms.com/api/Token'
+        headers = {
+            "Content-Type": "application/json",
         }
-        return self._contact_iap('/iap/sms/2/send', params)
+        body = {
+            "UserApiKey": UserApiKey,
+            "SecretKey": SecretKey
+        }
+        response = requests.post(url, data=json.dumps(body), headers=headers)
+        if response.status_code == 201:
+            if response.json()['IsSuccessful'] == True:
+                secure_token = response.json()['TokenKey']
+                print(secure_token)
+                print("This is Sucsess")
+                return secure_token
+            print(response.content)
+        return None
--- mobile_service_sms/wizards/res_config_settings.py
+++ mobile_service_sms/wizards/res_config_settings.py
@@ -6,25 +6,16 @@ from odoo import fields, models, api, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    server_special = fields.Boolean(string="Special Server")
+    server_special = fields.Boolean(
+        string="Special Server",
+        config_parameter='mobile_sms.server_special'
+    )
     server_id = fields.Many2one(
         comodel_name='mobile_sms.server',
         string="Server",
-        config_parameter='mobile_sms.server.name',
-        help="this is server sms")
-    
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            server_special=self.env['ir.config_parameter'].sudo().get_param('mobile_sms.server_special')
-        )
-        return res
-    
-    @api.model
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('mobile_sms.server_special', self.server_special)
+        config_parameter='mobile_sms.server_id',
+        help="this is server sms"
+    )
 
     def action_view_servers(self):
         self.ensure_one()
